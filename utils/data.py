7dd498e0e14ddb9bec475da2cfa794df2195c81d
unknown <grigorij.josifovski@gmail.com>
2024-07-30 00:01:39 +0200

    Update methods for merging dataframes

--- utils/data.py
+++ utils/data.py
@@ -1,9 +1,24 @@
+import logging
+
 import pandas as pd
 
 
-def merge_dataframes(df_curricula: pd.DataFrame, df_courses: pd.DataFrame) -> pd.DataFrame:
-    merged_data = df_curricula.set_index('course_code').join(df_courses.set_index('course_code'), how='inner', lsuffix='_curricula', rsuffix='_courses')
-    to_drop = [col for col in merged_data.columns if '_curricula' in col]
-    to_rename = {col: col.replace('_courses', '') for col in merged_data.columns if '_courses' in col}
-    return merged_data.drop(columns=to_drop).rename(columns=to_rename)
+def drop_and_rename_columns(df: pd.DataFrame) -> pd.DataFrame:
+    columns_to_drop = [col for col in df.columns if '_x' in col]
+    columns_to_rename = {col: col.replace('_y', '') for col in df.columns if '_y' in col}
+
+    return df.drop(columns=columns_to_drop).rename(columns=columns_to_rename)
+
+
+def merge_dataframes(df_curricula: pd.DataFrame, df_courses: pd.DataFrame, df_study_programs: pd.DataFrame) -> pd.DataFrame:
+    logging.info("Joining dataframes df_study_programs and df_curricula on study_program_name and study_program_duration")
+    merged_df = pd.merge(df_study_programs, df_curricula, how='inner', on=['study_program_name', 'study_program_duration'])
+    merged_df = drop_and_rename_columns(merged_df)
+
+    logging.info("Joining dataframes merged_df and df_courses on course_code")
+    merged_df = pd.merge(merged_df, df_courses, how='inner', on='course_code')
+    merged_df = drop_and_rename_columns(merged_df)
+
+    return merged_df
+
 
