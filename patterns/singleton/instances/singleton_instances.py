29f390417749ee469082150add2bd14c8f004f38
unknown <grigorij.josifovski@gmail.com>
2024-07-30 00:02:23 +0200

    Add AbstractSingleton, CourseNamesSingleton and CourseProfessorsSingleton

---
+++ patterns/singleton/instances/singleton_instances.py
@@ -0,0 +1,4 @@
+from patterns.singleton.model.singleton import CourseNamesSingleton, CourseProfessorsSingleton
+
+COURSE_NAMES_SINGLETON: CourseNamesSingleton = CourseNamesSingleton()
+COURSE_PROFESSORS_SINGLETON: CourseProfessorsSingleton = CourseProfessorsSingleton()
