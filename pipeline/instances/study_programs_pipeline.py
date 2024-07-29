6d91de66efc42ea6a46c0edc2ff08b479d18f78a
unknown <grigorij.josifovski@gmail.com>
2024-07-30 00:07:32 +0200

    Add steps to generate surrogate ids. Update run method

--- pipeline/instances/study_programs_pipeline.py
+++ pipeline/instances/study_programs_pipeline.py
@@ -9,10 +9,20 @@ study_programs_clean_data_pipeline = Pipeline(
     steps=[
         PipelineStep(name='clean_study_program_name', function=clean_study_program_name, source_columns=['study_program_name'],
                      destination_columns=['study_program_name']),
+    ],
+)
 
-    ]
+study_programs_generate_data_pipeline = Pipeline(
+    name='generate_study_programs_data',
+    steps=[
+        PipelineStep(name='generate_surrogate_study_program_id', function=None, source_columns=['study_program_name'],
+                     destination_columns=['study_program_id']),
+    ],
 )
 
 
-def run_study_programs_pipeline(df: pd.DataFrame) -> None:
+def run_study_programs_pipeline(df: pd.DataFrame) -> pd.DataFrame:
+    df.sort_values(by=['study_program_name', 'study_program_duration'], ignore_index=True, inplace=True)
     study_programs_clean_data_pipeline.run(df)
+    study_programs_generate_data_pipeline.run(df)
+    return df
