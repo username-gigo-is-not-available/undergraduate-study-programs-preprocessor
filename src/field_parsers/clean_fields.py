eeda8273788d1b9c718c985b7c2a8a73a91046ec
unknown <grigorij.josifovski@gmail.com>
2024-11-12 19:49:05 +0100

    Refactor to generic methods

---
+++ src/field_parsers/clean_fields.py
@@ -0,0 +1,34 @@
+from functools import cache
+
+from src.field_parsers.text_sanitizing_decorators import clean_whitespace, sentence_case, process_multivalued_field
+from src.config import Config
+
+
+@sentence_case
+@clean_whitespace
+@cache
+def clean_and_format_field(field: str) -> str:
+    return field
+
+
+@clean_whitespace
+@cache
+def clean_field(field: str) -> str:
+    return field
+
+
+@process_multivalued_field
+@cache
+def clean_and_format_multivalued_field(field: str) -> str:
+    return field
+
+
+@process_multivalued_field
+@cache
+def clean_professor_titles(course_professors: str) -> str:
+    def parse_professor_title(professor_name: str) -> str:
+        for title in Config.PROFESSOR_TITLES:
+            professor_name = professor_name.replace(title, "").strip()
+        return professor_name
+
+    return ', '.join(sorted((map(parse_professor_title, str(course_professors).split(", ")))))
