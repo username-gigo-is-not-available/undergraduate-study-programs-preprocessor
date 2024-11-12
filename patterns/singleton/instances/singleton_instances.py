0427e11d8f1aa7903fad75ef718a7ddacffcce75
unknown <grigorij.josifovski@gmail.com>
2024-11-12 20:12:03 +0100

    Remove singleton logic for matching course names and professors, now its done by DataTransformationMixin.apply_matching()

--- patterns/singleton/instances/singleton_instances.py
+++
@@ -1,4 +0,0 @@
-from patterns.singleton.model.singleton import CourseNamesSingleton, CourseProfessorsSingleton
-
-COURSE_NAMES_SINGLETON: CourseNamesSingleton = CourseNamesSingleton()
-COURSE_PROFESSORS_SINGLETON: CourseProfessorsSingleton = CourseProfessorsSingleton()
