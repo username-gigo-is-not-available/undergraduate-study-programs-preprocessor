544cce320d93400b883d9df494a7ed0358d6ee1f
unknown <grigorij.josifovski@gmail.com>
2025-03-14 14:38:17 +0100

    Rename steps

--- src/pipeline/common_steps.py
+++ src/pipeline/common_steps.py
@@ -1,8 +1,8 @@
-from src.pipeline.models.step import PipelineStep
+from src.patterns.builder.step import PipelineStep
 from src.field_parsers.clean_fields import clean_field, clean_and_format_field
 
 clean_study_program_name_step: PipelineStep = PipelineStep(
-    name='clean_study_program_name',
+    name='clean-study-program-name',
     function=PipelineStep.apply_function,
     mapping_function=clean_field,
     source_columns='study_program_name',
@@ -10,7 +10,7 @@ clean_study_program_name_step: PipelineStep = PipelineStep(
 )
 
 clean_course_code_step: PipelineStep = PipelineStep(
-    name='clean_course_code',
+    name='clean-course-code',
     function=PipelineStep.apply_function,
     mapping_function=clean_field,
     source_columns='course_code',
@@ -18,7 +18,7 @@ clean_course_code_step: PipelineStep = PipelineStep(
 )
 
 clean_course_name_mk_step: PipelineStep = PipelineStep(
-    name='clean_course_name_mk',
+    name='clean-course-name-mk',
     function=PipelineStep.apply_function,
     mapping_function=clean_and_format_field,
     source_columns='course_name_mk',
