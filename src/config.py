bede053032566d9a450170b423c9680b3905b0ef
grigo <grigorij.josifovski@gmail.com>
2025-06-27 22:23:49 +0200

    Fix columns

--- src/config.py
+++ src/config.py
@@ -115,12 +115,12 @@ class Config:
     PREREQUISITES_OUTPUT_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('PREREQUISITES_OUTPUT_FILE_NAME'))
     PREREQUISITES_OUTPUT_COLUMN_ORDER: list[str] = [
         'prerequisite_id',
-        'course_id',
+        'course_prerequisite_id',
         'requisite_id',
     ]
     POSTREQUISITES_OUTPUT_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('POSTREQUISITES_OUTPUT_FILE_NAME'))
     POSTREQUISITES_OUTPUT_COLUMN_ORDER: list[str] = [
         'postrequisite_id',
-        'course_prerequisite_id',
+        'course_id',
         'requisite_id',
     ]
